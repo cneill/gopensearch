9071a9a0ae3496fc76ad2d34e185e479df5802d7
Charles Neill <1749665+cneill@users.noreply.github.com>
2023-05-01 12:44:06 -0500

    adding some search engines

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		NewOpenSearchDescription(&OpenSearchDescriptionOpts{
 			ShortName:   "Search Go packages",
 			Description: "Search Go packages on pkg.go.dev",
-			Tags:        []string{"golang @golang whaaazaaa"},
+			Tags:        []string{"golang"},
 			URL: &OpenSearchURL{
 				Template: "https://pkg.go.dev/search?q={searchTerms}",
 				Type:     "text/html",
@@ -29,6 +29,66 @@ func main() {
 			},
 			Image: &OpenSearchImage{
 				FaviconDomain: "go.dev",
+				Width:         16,
+				Height:        16,
+			},
+		}).FillFavicon(),
+		NewOpenSearchDescription(&OpenSearchDescriptionOpts{
+			ShortName:   "Search deps.dev",
+			Description: "Search for package dependencies/versions on deps.dev",
+			Tags:        []string{"vulnerabilities"},
+			URL: &OpenSearchURL{
+				Template: "https://deps.dev/search?q={searchTerms}",
+				Type:     "text/html",
+				Method:   "get",
+			},
+			Image: &OpenSearchImage{
+				FaviconDomain: "deps.dev",
+				Width:         16,
+				Height:        16,
+			},
+		}).FillFavicon(),
+		NewOpenSearchDescription(&OpenSearchDescriptionOpts{
+			ShortName:   "Search kubernetes.io",
+			Description: "Search for documentation on the Kubernetes site",
+			Tags:        []string{"k8s"},
+			URL: &OpenSearchURL{
+				Template: "https://kubernetes.io/search/?q={searchTerms}",
+				Type:     "text/html",
+				Method:   "get",
+			},
+			Image: &OpenSearchImage{
+				FaviconDomain: "kubernetes.io",
+				Width:         16,
+				Height:        16,
+			},
+		}).FillFavicon(),
+		NewOpenSearchDescription(&OpenSearchDescriptionOpts{
+			ShortName:   "Terraform module search",
+			Description: "Search for Terraform modules on registry.terraform.io",
+			Tags:        []string{"terraform"},
+			URL: &OpenSearchURL{
+				Template: "https://registry.terraform.io/search/modules?q={searchTerms}",
+				Type:     "text/html",
+				Method:   "get",
+			},
+			Image: &OpenSearchImage{
+				FaviconDomain: "terraform.io",
+				Width:         16,
+				Height:        16,
+			},
+		}).FillFavicon(),
+		NewOpenSearchDescription(&OpenSearchDescriptionOpts{
+			ShortName:   "Terraform provider search",
+			Description: "Search for Terraform providers on registry.terraform.io",
+			Tags:        []string{"terraform"},
+			URL: &OpenSearchURL{
+				Template: "https://registry.terraform.io/search/providers?q={searchTerms}",
+				Type:     "text/html",
+				Method:   "get",
+			},
+			Image: &OpenSearchImage{
+				FaviconDomain: "terraform.io",
 				Width:         16,
 				Height:        16,
 			},
